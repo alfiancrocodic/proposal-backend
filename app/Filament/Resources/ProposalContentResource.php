2efce08cbae45d9a1f715cb7db9a1e12bd8db59c
Alfian Nur Rahmad <alfianptik@gmail.com>
2025-09-14 21:11:52 +0700

    feat(resources): add delete action to client, project, and proposal resources

--- app/Filament/Resources/ProposalContentResource.php
+++ app/Filament/Resources/ProposalContentResource.php
@@ -52,6 +52,7 @@ class ProposalContentResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
